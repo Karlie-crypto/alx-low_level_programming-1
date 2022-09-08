827a12aacb96c7e10bf62e65e40716c2632d9157
Young-prof <frankiperezi99@gmail.com>
2022-09-08 12:59:54 -0700

    Task 3 and 4, C programms

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - print the string in the put fuction
+ * this program prints "programming is like building a multilingual puzzle"
+ * return: 0
+ * /
+ int main (void)
+ { puts(*\"programming is like building a multilingual puzzle*);
+   return (0);
+ }
