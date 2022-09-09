78f9b997695efcaa79d62c071322947143b4e00a
Young-prof <frankiperezi99@gmail.com>
2022-09-09 16:24:42 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints numbers between 0 to 99.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, e, g, h, op1, op2;
-
 i = e = g = h = 48;
 while (h < 58)
 {
