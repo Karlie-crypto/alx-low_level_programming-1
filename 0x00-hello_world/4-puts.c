2028c742d3aa12fa5ddb41175a07a6a0ee9616b5
Young-prof <frankiperezi99@gmail.com>
2022-09-08 17:07:55 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>                                           
 
-/**                                                          
- * main - using the put function to print string             
- *                                                           
- * Description: using the main function to print             
- * Programming is like building a multilingual puzzle       
+/**
+ * main - using the put function to print string
+ *
+ * Description: using the main function to print
+ * Programming is like building a multilingual puzzl
  * Return: 0                                                
  */                                                          
 int main(void)                                               
