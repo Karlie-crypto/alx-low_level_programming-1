f512315f73b0207ed75e245f0e45c47482b158ea
Young-prof <frankiperezi99@gmail.com>
2022-09-26 23:02:24 +0100

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,10 +2,10 @@
 #define NULL 0
 
 /**
- * _strstr - locate and return pointer to first occurence of substring
- * @haystack: string to search
- * @needle: target substring to search for
- * Return: pointer to index of string at first occurence of whole substring
+ *   _strstr - locate and return pointer to first occurence of substring
+ *    @haystack: string to search
+ *     @needle: target substring to search for
+ *  Return: pointer to index of string at first occurence of whole substring
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -14,6 +14,7 @@ char *_strstr(char *haystack, char *needle)
 
     if (needle[0] == '\0')
         return (haystack);
+
     while (haystack[i] != '\0') /* iterate through haystack */
     {
         /* if a byte matches first char of needle */
