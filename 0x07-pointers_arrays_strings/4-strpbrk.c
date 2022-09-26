baacbd4f62d8243406160c49bb7dedc2c9256af8
Young-prof <frankiperezi99@gmail.com>
2022-09-26 14:24:13 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,19 +10,19 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-    int x = 0, y;
-    while (s[x] != '\0')
-    {
-        for (y = 0; accept[y] != '\0'; y++)
-        {
-            if (s[x] == accept[y])
-            {
-                s = &s[x];
-                return (s);
-            }
-        }
-        x++;
-    }
+	int x = 0, y;
 
-    return (NULL);
+	while (s[x] != '\0')
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				s = &s[x];
+				return (s);
+			}
+		}
+	x++;
+	}
+return (NULL);
 }
