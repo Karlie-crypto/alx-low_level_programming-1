a8f7bd58f0b1422ae7b0655f82e5291a0fe370d1
Young-prof <frankiperezi99@gmail.com>
2022-09-28 13:20:16 -0700

    task 7 correction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,7 @@ int is_prime_number(int n);
 int _sqrt(int n, int x);
 int is_prime(int n, int start);
 void check(char *s, int start, int end, int *flag);
+int int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
