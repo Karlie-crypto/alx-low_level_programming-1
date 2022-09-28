35e06ef1d56ea8cde71ab1217c9a47e1e6cde099
Young-prof <frankiperezi99@gmail.com>
2022-09-28 03:51:15 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,24 @@
  *
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
+
 	if (*s == '\0')
+
 	{
+
 		_putchar('\n');
+
+		return;
+
 	}
-	else
-	{
-		_putchar(s[0]);
-		_puts_recursion(s + l);
-	}
+
+	_putchar(*s);
+
+	s++;
+
+	_puts_recursion(s);
+
 }
