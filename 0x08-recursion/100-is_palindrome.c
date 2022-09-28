73065eea7fee51a5b0e9d2b6f5a4c53564b7a945
Young-prof <frankiperezi99@gmail.com>
2022-09-28 13:53:14 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,56 +1,83 @@
-#include "main.h"
-
 /**
-* is_palindrome - returns the 1 if s is a palindrome
-* @s: string to be checked
-*
-* Return: 1 if s is a palindrome. 0 otherwise
+
+* substring_match - check if a substring after wildcard matches s1
+
+* @s1: one string
+
+* @s2: one string
+
+* @after_wldcd: placeholder for position right after wildcard
+
+* Return: 1 if matched, 0 if not
+
 */
 
-int int is_palindrome(char *s)
-{
-	int flag = 1;
 
-	check(s, 0, _strlen_recursion(s) - 1, *flag);
-	return (flag);
+
+int substring_match(char *s1, char *s2, char *after_wldcd)
+
+{
+
+if (*s1 == '\0' && *s2 == '\0')
+
+return (1);
+
+if (*s1 == '\0' && *s2 == '*')
+
+return (substring_match(s1, s2 + 1, s2 + 1));
+
+if (*s1 == '\0' && *s2 != '\0')
+
+return (0);
+
+if (*s2 == '*')
+
+return (substring_match(s1, s2 + 1, s2 + 1));
+
+if (*s1 == *s2)
+
+return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+
+else
+
+return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+
 }
-/**
-* check - checks if a string is a palindrome
-* @s: string to be checked
-* @start: start index
-* @end: end index
-* @flag: flag to indicate if a string is a palindrome
-*
-* Return: void
-*/
 
-void check(char *s, int start, in end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
-	}
-}
+
 
 /**
-* _strlen_recursion - calculates the length of a string
-* @s: string to be void
-*
-* Return: length of the string
+
+* wildcmp - compare if string with wildcard mattches
+
+* @s1: one string
+
+* @s2: one string
+
+* Return: 1 if matched, 0 if not
+
 */
 
-int _strlen_recursion(char *s)
-{
-	int sum - 0;
 
-	if (*s != '\0')
-	{
-		sum++;
-		sum + -_strlen_recursion(s + 1);
-	}
-	return (sum);
+
+int wildcmp(char *s1, char *s2)
+
+{
+
+if (*s1 == '\0' && *s2 == '\0')
+
+return (1);
+
+if (*s1 == *s2)
+
+return (wildcmp(s1 + 1, s2 + 1));
+
+else if (*s2 == '*')
+
+return (substring_match(s1, (s2 + 1), (s2 + 1)));
+
+else
+
+return (0);
+
 }
