bf1f1c99f9d800f5b6bad1ab9e8b8df95ddc4160
Young-prof <frankiperezi99@gmail.com>
2022-09-30 17:20:16 -0700

    correction made

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,11 +2,11 @@
 #define MAIN_H
 
 /*
- *  * File: main.h
- *   * Auth: Alagwu David
- *    * Desc: Header file containing the prototypes of all functions
- *     *       included in the static library libholberton.a.
- *      */
+ * File: main.h
+ * Auth: Tekenatei Franklyn
+ * Desc: Header file containing the prototypes of all functions
+ * included in the static library libholberton.a.
+ */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
